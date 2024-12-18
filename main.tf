78760629a785456d386f84d63eaa84de8857179d
MV NAGA BHARATH KUMAR <113744592+mvnbharath@users.noreply.github.com>
2024-12-18 19:51:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,61 @@
-
-data "aws_vpc" "default" {
-  default = true
+provider "aws" {
+  region = "us-east-1"
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-data "aws_security_group" "default" {
-  filter {
-    name   = "group-name"
-    values = ["default"]
-  }
-}
-
-resource "aws_instance" "amazon_linux" {
-  ami                    = "ami-0614680123427b75e" 
-  instance_type          = "t2.micro"             
-  key_name               = "kiki" 
-  subnet_id              = data.aws_subnets.default.ids[0] 
-  vpc_security_group_ids = [data.aws_security_group.default.id] 
-
+resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
+  ami                    = "ami-053b12d3152c0cc71"
+  instance_type          = "t2.micro" 
+  key_name               = "kiki"
   tags = {
-    Name = "amazon_linux_vm"
+    Name = "u21.local"
   }
-
-  
   user_data = <<-EOF
-              #!/bin/bash
-              hostnamectl set-hostname c8.local
-              echo "c8.local" > /etc/hostname
-              EOF
+  #!/bin/bash
+  sudo hostnamectl set-hostname U21.local
+  # netdata_conf="/etc/netdata/netdata.conf"
+  # Path to netdata.conf
+  # actual_ip=0.0.0.0
+  # Use sed to replace the IP address in netdata.conf
+  # sudo sed -i "s/bind socket to IP = .*$/bind socket to IP = $actual_ip/" "$netdata_conf"
+EOF
+
 }
 
-
-resource "aws_instance" "ubuntu_2104" {
-  ami                    = "ami-053b12d3152c0cc71" 
-  instance_type          = "t2.micro"             
-  key_name               = "kiki" 
-  subnet_id              = data.aws_subnets.default.ids[0] 
-  vpc_security_group_ids = [data.aws_security_group.default.id] 
-
+resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
+  ami                    = "ami-0614680123427b75e"
+  instance_type          = "t2.micro"
+  key_name               = "kiki"
   tags = {
-    Name = "ubuntu_21_04_vm"
+    Name = "c8.local"
   }
-
-  
   user_data = <<-EOF
-              #!/bin/bash
-              hostnamectl set-hostname u21.local
-              echo "u21.local" > /etc/hostname
-              EOF
+  #!/bin/bash
+  # New hostname and IP address
+  sudo hostnamectl set-hostname u21.local
+  hostname=$(hostname)
+  public_ip="$(curl -s https://api64.ipify.org?format=json | jq -r .ip)"
+
+  # Path to /etc/hosts
+  echo "${aws_instance.backend.public_ip} $hostname" | sudo tee -a /etc/hosts
+
+EOF
+depends_on = [aws_instance.backend]
+}
+
+resource "local_file" "inventory" {
+  filename = "./inventory.yaml"
+  content  = <<EOF
+[frontend]
+${aws_instance.frontend.public_ip}
+[backend]
+${aws_instance.backend.public_ip}
+EOF
+}
+
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
 }
